4add4bbf5677b78ba309a5994efa7b2e8e85d6fb
Saurabh <sourabhpatil310@gmail.com>
2023-07-14 11:44:03 +0530

    Model Building & fitting

--- maintrain.py
+++ maintrain.py
@@ -1,8 +1,14 @@
 import cv2
 import os
+import tensorflow as tf
+from tensorflow import keras
 from PIL import Image
 import numpy as np
 from sklearn.model_selection import train_test_split
+from keras.utils import normalize
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPool2D
+from keras.layers import  Activation, Dropout, Dense, Flatten
 
 image_directory='datasets/'
 
@@ -11,6 +17,7 @@ yes_tumor_imges= os.listdir(image_directory+ 'yes/')
 
 dataset=[]
 label=[]
+INPUT_SIZE=64
 
 # print(no_tumor_imges)
 
@@ -23,7 +30,7 @@ for i , image_name in enumerate(no_tumor_imges):
     if(image_name.split('.')[1]=='jpg'):
         image=cv2.imread(image_directory+ 'no/'+image_name)
         image=Image.fromarray(image, 'RGB')
-        image=image.resize((64,64))
+        image=image.resize((INPUT_SIZE,INPUT_SIZE))
         dataset.append(np.array(image))
         label.append(0)
 
@@ -31,7 +38,7 @@ for i , image_name in enumerate(yes_tumor_imges):
     if(image_name.split('.')[1]=='jpg'):
         image=cv2.imread(image_directory+ 'yes/'+image_name)
         image=Image.fromarray(image, 'RGB')
-        image=image.resize((64,64))
+        image=image.resize((INPUT_SIZE,INPUT_SIZE))
         dataset.append(np.array(image))
         label.append(1)
     
@@ -48,12 +55,45 @@ label=np.array(label)
 x_train, x_test, y_train, y_test=train_test_split(dataset,label, test_size=0.2, random_state=0)
 
 # Reshape = (n, image_width, image_height, n_channel)(2400,64,64,3)
-print(x_train.shape)
-print(y_train.shape)
-print(x_test.shape)
-print(y_test.shape)
+# print(x_train.shape)
+# print(y_train.shape)
+# print(x_test.shape)
+# print(y_test.shape)
 
+#Normalizing the data for training
 
+x_train=normalize(x_train,axis=1)
+x_test=normalize(x_test,axis=1)
 
+#Model Building 
+# 64,64,3
+ 
+model= Sequential()
 
+model.add(Conv2D (32,(3,3), input_shape=(INPUT_SIZE,INPUT_SIZE,3))) #Adding first layer
+model.add(Activation('relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
 
+model.add(Conv2D (32,(3,3), kernel_initializer='he_uniform'))
+model.add(Activation('relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+
+model.add(Conv2D (64,(3,3), kernel_initializer='he_uniform'))
+model.add(Activation('relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))  #1-bcos op is yes and no which is binary classification problem
+model.add(Activation('sigmoid')) #Activation function
+
+# Binary CrossEntropy = 1, sigmoid
+# Categorical CrossEntropy = 2, softmax
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(x_train,y_train,batch_size= 16,verbose=1,epochs=10,validation_data=(x_test,y_test ),shuffle=False)
+
+model.save('Braintumor10Epochs.h5')
